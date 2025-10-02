008045529c0c64fe3fd73f3db951d2f2cc672e33
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-02 05:34:53 +0530

    Add files via upload

---
+++ BasicsOfJava/areaofcircle.java
@@ -0,0 +1,9 @@
+package basicsofjava;
+
+public class areaofcircle {
+    public static void main(String[] args) {
+        double r=3.7;
+        double a= 3.141592*r*r;
+        System.out.println("area of circle with radius" +r+ "is" +a);
+    }
+}
