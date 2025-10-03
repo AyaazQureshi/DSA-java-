626be0b9140daebec4a7b292dc8ef6ae1ba656f3
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-03 19:06:44 +0530

    Add files via upload

---
+++ BasicsOfJava/basics.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("college wallah");
+        System.out.println("upsc wallah");
+        System.out.println("ssc wallah");
+        System.out.println("banking wallah");
+        System.out.println(4);
+        System.out.println("hello world");
+
+
+
+    }
+}
