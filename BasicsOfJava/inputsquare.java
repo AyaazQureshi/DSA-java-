5131ad6a39edce0e68cbe1adffb43d2a618720ad
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-04 14:07:40 +0530

    Add files via upload

---
+++ BasicsOfJava/inputsquare.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+ import java.sql.SQLOutput;
+ import java.util.Scanner;
+public class inputsquare {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.print("enter a no : ");
+        int n = sc.nextInt();
+        System.out.println(n*n);
+
+    }
+}
