9624e8f8403bd819c15a0c26e6c1d08c7aae2e51
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-05 07:30:30 +0530

    Add files via upload

---
+++ BasicsOfJava/relationaloperator.java
@@ -0,0 +1,9 @@
+package basicsofjava;
+
+public class relationaloperator {
+    public static void main(String[] args) {
+        int a =5;
+        int b =5;
+        System.out.println(a==b);
+    }
+}
