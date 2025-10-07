f878ea79d71065f6436c414b2dcbba52b613cf4a
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-07 17:48:22 +0530

    Add files via upload

---
+++ BasicsOfJava/typecasting.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+ import java.util.Scanner;
+public class typecasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a no: ");
+        int x = sc.nextInt();
+        System.out.print((double)x/2);
+
+
+    }
+}
