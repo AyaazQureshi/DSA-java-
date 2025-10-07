d2cb81354b6206a6c6886b7f9d7809db98bf9262
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-07 17:48:47 +0530

    Add files via upload

---
+++ BasicsOfJava/updatingvariables.java
@@ -0,0 +1,10 @@
+package basicsofjava;
+
+public class updatingvariables {
+    public static void main(String[] args) {
+        int x = 8;
+        System.out.println(x);
+        x=10;
+        System.out.println(x);
+    }
+}
