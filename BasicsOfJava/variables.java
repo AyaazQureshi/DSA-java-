c0ca3ac64ffe09cca73d331eb2502aa26ca13a0c
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-08 17:45:27 +0530

    Add files via upload

---
+++ BasicsOfJava/variables.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+
+public class variables {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x);
+        System.out.println("x");
+        int y = 8; //declaration + initialization...
+        System.out.println(y);
+        System.out.println(x+y);
+    }
+}
