2cc2f775e566df1907af6cbce22983d2dba4d40b
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-08 17:46:04 +0530

    Add files via upload

---
+++ BasicsOfJava/volumeofsphere.java
@@ -0,0 +1,10 @@
+package basicsofjava;
+
+public class volumeofsphere {
+    public static void main(String[] args) {
+        double r= 3.5;
+        double v= (4* 3.141592*r*r*r)/3;
+        System.out.println("the volume of sphere with radius " +r+ " is "+v);
+
+    }
+}
