684983085565d19f6c084e28133c13929af4a1ab
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-14 08:11:20 +0530

    Add files via upload

---
+++ conditions/profitloss.java
@@ -0,0 +1,19 @@
+package conditionals;
+ import java.util.Scanner;
+public class profitloss {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter cost price: ");
+        int cp = sc.nextInt();
+        System.out.println("enter selling price: ");
+        int sp = sc.nextInt();
+        if(sp>cp)
+            System.out.println("profit is "+(sp-cp));
+        else if(sp == cp)
+            System.out.println("no profit no loss");
+        else
+            System.out.println("loss is "+(cp-sp));
+
+
+    }
+}
