86ecbe0d46324b8d0431211f9780fec7e6136b03
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-17 08:28:38 +0530

    Add files via upload

---
+++ conditions/useoflogicalor.java
@@ -0,0 +1,14 @@
+package conditionals;
+ import java.util.Scanner;
+public class useoflogicalor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+        if (n%5==0 || n%3==0)
+            System.out.println("it is divisible by 5 or 3");
+        else
+            System.out.println(" it is not divisible by 5 or 3");
+
+    }
+}
