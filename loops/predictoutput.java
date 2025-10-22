5c2d4cdce1967391710d9cc87f16d7b8dda22154
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-22 13:03:45 +0530

    Add files via upload

---
+++ loops/predictoutput.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class predictoutput {
+    public static void main(String[] args) {
+        int i = 10;
+        while(i == 20){
+            System.out.println("a complete buff!");
+
+        }
+    }
+}
