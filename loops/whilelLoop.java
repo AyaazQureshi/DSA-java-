fbb429b1aa1b0ac690f4c1e74df6867f68f1121b
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-25 23:21:51 +0530

    Add files via upload

---
+++ loops/whilelLoop.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class whilelLoop {
+    public static void main(String[] args) {
+ // u can use for loop for same...
+        int i= 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
