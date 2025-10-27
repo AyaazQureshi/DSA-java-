468c4a053647facb047354a5bc9366341ba9a541
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-10-27 11:37:30 +0530

    Add files via upload

---
+++ methods/Basic.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Basic {
+    static void greeting(){
+        System.out.print("pw op");
+    }
+    public static void main(String[] args) {
+
+        greeting();
+    }
+}
