a8b7399ba7b82294856c30ba4db7488ec009b5a5
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-11-07 18:48:07 +0530

    Add files via upload

---
+++ arrays/basicSyntax.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class basicSyntax {
+    public static void main(String[] args) {
+        //Method1....
+        int[] arr = new int[5];
+        arr[0] = 190;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
+// method2......
+//package Arrays;
+//
+//public class basicSyntax {
+//    public static void main(String[] args) {
+//        int[] arr = {190,20,30,40,50};
+//
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[3]);
+//        System.out.println(arr[4]);
+//
+//   }
+//}
