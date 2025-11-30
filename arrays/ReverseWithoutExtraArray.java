24cb2b8d535290b4d6668ba660996eca2e2a52a1
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2025-11-30 12:38:11 +0530

    Add files via upload

---
+++ arrays/ReverseWithoutExtraArray.java
@@ -0,0 +1,21 @@
+package arrays;
+public class ReverseWithoutExtraArray {
+    public static void main(String[] args) {
+        int[] arr ={10,20,30,40,50,60,70};
+        int n=arr.length;
+
+        // reverse...
+           int i=0,j=n-1;
+           while(i<j){
+               int temp = arr[i];
+               arr[i]=arr[j];
+               arr[j]=temp;
+               i++; j--;
+
+           }
+            for(int ele : arr){
+                System.out.print(ele+" ");
+            }
+        }
+    }
+
