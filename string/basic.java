e1f20a08b2168d8c72f41dee825ad8d7354b0beb
Qureshi Ayaaz <179160854+AyaazQureshi@users.noreply.github.com>
2026-02-10 11:17:28 +0530

    Add files via upload

---
+++ string/basic.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str =" naruto uzumakhi!!!";
+        System.out.print(str);
+//         input..
+        String s  = sc.nextLine();
+        System.out.print(s);
+    }
+}
